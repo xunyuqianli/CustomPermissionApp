a22de708ea4d4a4ea125276406b86881a8cece53
xunyuqiali <274596545@qq.com>
2017-02-06 11:22:25 +0800

    访问有自定义权限的别的app的activity

---
+++ app/src/main/java/com/example/a27459/apptest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a27459.apptest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) {
+        //隐式启动
+        Intent intent = new Intent("scott.intent.action.MY_CALL");
+        startActivity(intent);
+    }
+}
